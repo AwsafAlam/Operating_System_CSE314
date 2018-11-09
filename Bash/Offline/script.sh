bd6ab7036eb6d17f62dbb57c3c6341c75f901142
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-11-09 22:09:26 +0600

    Shell script offlien1, done setting absents list, and marks for all

--- Bash/Offline/script.sh
+++ Bash/Offline/script.sh
@@ -8,7 +8,37 @@ clear
 echo "" > filelist.txt
 for variable in $var
 do
-    echo "-- $variable --">>filelist.txt
+    echo "$variable" >> filelist.txt
 done
-grep -E "[0-9]*" CSE_322.csv
-# grep -v "^#" CSE_322.csv > output.csv
+# grep -wE "[0-9]*" filelist.txt
+# grep -w -E "0-9" CSE_322.csv
+# grep -P "\d{7}" CSE_322.csv > output.csv
+
+# cut -d"," -f1 CSE_322.csv | cut -d"\"" -f2
+# cut -d "\"" -f2 CSE_322.csv
+
+# cut -d "_" -f5 -s filelist.txt | cut -d "." -f1  ## Getting list of roll numbers from the extracted files
+
+filelist=`cut -d "_" -f5 -s filelist.txt | cut -d "." -f1`
+Raster=`cut -c 3-9 CSE_322.csv`
+
+for item in $Raster
+do
+flag=0
+    # echo $item
+    for file in $filelist 
+    do
+        if [ $item = $file ] 
+        then
+            flag=1
+            echo "$file 10" >> Marks.txt
+            break
+        fi
+    done
+
+    if [ $flag = 0 ] 
+    then
+        echo $item >> Absents.txt
+        echo "$item 0" >> Marks.txt
+    fi
+done
