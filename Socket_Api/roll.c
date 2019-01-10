07d7826a0e4942661701d36e3ff2257e532e5cf1
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-01-10 15:16:15 +0600

    Added Socket api project

---
+++ Socket_Api/roll.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+    printf(1, "Printing inside user program - \nRoll: %d\n",awsaf_alam());
+    // awsaf_alam();
+    exit();
+}
