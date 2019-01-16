1f20aacfbdf9f54e4290762cbdf89ac64c58a23c
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-01-16 11:58:21 +0600

    After online

--- Socket_Api/roll.c
+++ Socket_Api/roll.c
@@ -1,11 +1,10 @@
 #include "types.h"
-//#include "stat.h"
 #include "user.h"
 
 int
 main(int argc, char *argv[])
 {
-    printf(1, "Printing inside user program - \nRoll: %d\n",awsaf_alam());
+    printf(1,"Printing inside user program - \nRoll: %d\n",awsaf_alam());
     // awsaf_alam();
     exit();
 }
